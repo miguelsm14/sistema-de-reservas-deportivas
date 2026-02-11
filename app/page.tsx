59ea2f7697bbb75f48782f772e515d7253a46449
Miguel Sanchez <miguelsanchezmaraver@gmail.com>
2026-02-11 21:07:25 +0100

    11/02

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-10 items-center">
+    <main className="min-h-screen flex-col items-center ">
+      <div className="flex-1 flex-col gap-10 items-center">
         {/*ESTO ES EL HEADER */}
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
@@ -41,7 +41,7 @@ export default function Home() {
         {/*AQUI TERMINA EL HEADER */}
 
         {/*AQUI DENTRO VA LA INFORMACIÓN QUE QUIERO QUE TENGA LA PÁGINA*/}
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+        <div className="">
           <Hero />
         </div>
         {/*AQUI TERMINA LA INFORMACIÓN QUE QUIERO QUE TENGA LA PÁGINA*/}
--- components/hero.tsx
+++ components/hero.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export function Hero() {
   return (
-    <div className="w-full max-w-7xl mx-auto px-4">
+    <div className="items-center">
 
       {/* ================= HERO ================= */}
-      <section className="flex flex-col lg:flex-row items-center gap-12">
+      <section className="flex max-w-7xl mx-auto px-4 sm:px-6 w-full pt-40">
 
         {/* IZQUIERDA */}
         <div className="w-full lg:w-1/2 flex flex-col gap-6 text-center lg:text-left">
@@ -19,7 +19,7 @@ export function Hero() {
         </div>
 
         {/* DERECHA */}
-        <div className="w-full lg:w-1/2 flex justify-center">
+        <div className="w-full lg:w-1/2 flex justify-end">
           <Image
             src="/reserva.png"
             alt="Imagen Hero"
@@ -32,21 +32,27 @@ export function Hero() {
 
       {/* ================= TITULO NEGATIVO ================= */}
       <section className="mt-24 text-center">
+        <p className="text-muted-foreground text-red-500">
+          EL RELOJ DEL PASADO
+        </p>
         <h2 className="text-3xl lg:text-4xl font-bold">
           Reservar una pista sigue siendo un lío
         </h2>
       </section>
 
       {/* ================= DESVENTAJAS ================= */}
-      <section className="mt-12 flex flex-col lg:flex-row gap-8">
+      <section className="flex max-w-7xl mx-auto px-4 sm:px-6 pt-40 gap-8">
 
         {/* CARD 1 */}
-        <div className="w-full lg:w-1/3 flex flex-col gap-4 text-center">
+        <div className="w-full lg:w-1/3
+          flex flex-col gap-4 text-center 
+          border border-red-300 rounded-xl p-6 
+          transition-all duration-300 ease-out hover:shadow-xl hover:-translate-y-2">
           <h3 className="text-2xl font-bold">Dependencia total</h3>
           <p className="text-muted-foreground">
             La reserva depende de una persona, no de un sistema.
           </p>
-          <div className="flex justify-center">
+          <div className="flex justify-center mt-auto">
             <Image
               src={"/reserva.png"}
               alt=""
@@ -57,12 +63,15 @@ export function Hero() {
         </div>
 
         {/* CARD 2 */}
-        <div className="w-full lg:w-1/3 flex flex-col gap-4 text-center">
-          <h3 className="text-2xl font-bold">Papel = errores</h3>
+        <div className="w-full lg:w-1/3
+          flex flex-col gap-4 text-center
+          border border-red-300 rounded-xl p-6 
+          transition-all duration-300 ease-out hover:shadow-xl hover:-translate-y-2">
+          <h3 className="text-2xl font-bold">Imposible Escalar</h3>
           <p className="text-muted-foreground">
-            Un papel no valida nada. <br/> Un ordenador si.
+            No crece, solo aguanta.
           </p>
-          <div className="flex justify-center">
+          <div className="flex justify-center mt-auto">
             <Image
               src={"/reserva.png"}
               alt=""
@@ -73,12 +82,15 @@ export function Hero() {
         </div>
 
         {/* CARD 3 */}
-        <div className="w-full lg:w-1/3 flex flex-col gap-4 text-center">
-          <h3 className="text-2xl font-bold">Todo depende de memoria humana</h3>
+        <div className="w-full lg:w-1/3
+          flex flex-col gap-4 text-center
+          border border-red-300 rounded-xl p-6 
+          transition-all duration-300 ease-out hover:shadow-xl hover:-translate-y-2">
+          <h3 className="text-2xl font-bold">Cero Seguridad</h3>
           <p className="text-muted-foreground">
-            La memoria falla. Siempre.
+            Todo es "palabra contra palabra"
           </p>
-          <div className="flex justify-center">
+          <div className="flex justify-center mt-auto">
             <Image
               src={"/reserva.png"}
               alt=""
@@ -90,6 +102,95 @@ export function Hero() {
 
       </section>
 
+      {/* ================= TITULO POSITIVO ================= */}
+      <section className="mt-24 text-center">
+        <p className="text-green-500">
+          MODO AUTOMÁTICO
+        </p>
+        <h2 className="text-3xl lg:text-4xl font-bold">
+          Menos llamadas, menos errores, menos fricción.
+        </h2>
+      </section>
+
+      {/* ================= VENTAJAS ================= */}
+      <section className="flex max-w-7xl mx-auto px-4 sm:px-6 pt-40 gap-8">
+
+        {/* CARD 1 */}
+        <div className="w-full lg:w-1/3
+          flex flex-col gap-4 text-center
+          border border-green-300 rounded-xl p-6 
+          transition-all duration-300 ease-out hover:shadow-xl hover:-translate-y-2">
+          <h3 className="text-2xl font-bold">Reserva instantánea</h3>
+          <p className="text-muted-foreground">
+            Reserva en segundos.<br /> Reserva cuando quieras.
+          </p>
+          <div className="flex justify-center mt-auto">
+            <Image
+              src={"/reserva.png"}
+              alt=""
+              width={150}
+              height={150}
+            />
+          </div>
+        </div>
+
+        {/* CARD 2 */}
+        <div className="w-full lg:w-1/3
+          flex flex-col gap-4 text-center
+          border border-green-300 rounded-xl p-6 
+          transition-all duration-300 ease-out hover:shadow-xl hover:-translate-y-2">
+          <h3 className="text-2xl font-bold">Disponible 24/7</h3>
+          <p className="text-muted-foreground">
+            El deporte no tiene horarios,<br />la app tampoco.
+          </p>
+          <div className="flex justify-center mt-auto">
+            <Image
+              src={"/reserva.png"}
+              alt=""
+              width={150}
+              height={150}
+            />
+          </div>
+        </div>
+
+        {/* CARD 3 */}
+        <div className="w-full lg:w-1/3
+          flex flex-col gap-4 text-center
+          border border-green-300 rounded-xl p-6 
+          transition-all duration-300 ease-out hover:shadow-xl hover:-translate-y-2">
+          <h3 className="text-2xl font-bold">Seguridad y Control</h3>
+          <p className="text-muted-foreground">
+            Todo registrado, sin discusiones.
+          </p>
+          <div className="flex justify-center mt-auto">
+            <Image
+              src={"/reserva.png"}
+              alt=""
+              width={150}
+              height={150}
+            />
+          </div>
+        </div>
+
+      </section>
+
+      {/* ================= TITULO 2VS2 ================= */}
+      <section className="mt-24 text-center border-t bg-emerald-300">
+        <p className="text-green-500 pt-6">
+          PADEL
+        </p>
+        <h2 className="text-3xl lg:text-4xl font-bold pb-6 text-white">
+          Modo 2vs2 aleatorio activado
+        </h2>
+
+        <div className=" bg-emerald-300 pb-6">
+          <p className="mx-auto text-lg mb-6 w-px-3">
+            A parte de la facilidad para reservar pistas deportivas, tambien nos aseguramos que te lo pases bien haciendo tu deporte favorito.<br />
+            El modo 2vs2 se basa en una reserva a mitad de precio, pero con la condición de que no se reserva hasta que otra pareja aleatoria reserve la otra mitad de la pista.<br />
+            Haciendo así que si no tienes con quien jugar, puedas enfrentarte a otra pareja aleatoriamente.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
